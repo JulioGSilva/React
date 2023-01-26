ac251cb71bcac3d6be1c21cfc38ad3cbd1a0442a
JulioGSilva <113954423+JulioGSilva@users.noreply.github.com>
2023-01-25 22:11:33 -0300

    exercicio 6 do ifis

--- meu-projeto/src/components/Form.js
+++ meu-projeto/src/components/Form.js
@@ -9,10 +9,12 @@ function Form() {
             <h1>Meu Cadastro:</h1>
             <form onSubmit={cadastrarUsuario} action="" method="post">
                 <div>
+                    <label htmlFor="name">Nome:</label>
                     <input type="text" placeholder="Digite seu nome"/>
                 </div>
                 <br />
                 <div>
+                    <label htmlFor="passowrd">Senha:</label>
                     <input type="password" name="senha" id="senha" placeholder="Digite sua senha" />
                 </div>
                 <div>
